0c8b0fd0ff11377a696cce144779f56a602064fb
Hericles Henrique <programmer.linux.ti@gmail.com>
2021-11-01 19:03:01 -0300

    Update script.js

--- Troca de Humor/script.js
+++ Troca de Humor/script.js
@@ -6,17 +6,17 @@ const botao = document.querySelector  ("#btn");
 
 btn.addEventListener ("click", function () { 
     if (botao.value  == 'primeiro'){
-        TX.innerText = "texto\n medo\n asdfasdfasdo!"
+        TX.innerText = "Html"
         botao.value  = 'segundo'
         img.src      = "img/html.png"   }
 
     else if (botao.value == "segundo"){
-        TX.innerText = "texto\n qwerqeqrwe\n asdfasdfasdo!"
+        TX.innerText = "JavaScript"
         botao.value  = "terceiro"
         img.src      = "img/js.png" }
 
     else {
-        TX.innerText = "texto\n dafdasfa\n asdfasdfasdo!"
+        TX.innerText = "Css"
         botao.value  = "primeiro"
         img.src      = "img/css.png"}
   });
